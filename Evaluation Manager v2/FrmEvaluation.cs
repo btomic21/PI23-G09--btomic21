04ab4c25b82860cd235c5be7f46f8ae1623461e5
Ariana Slaviček <aslavicek21@foi.hr>
2023-05-09 14:59:48 +0200

    BT_forma_evaluacija

---
+++ Evaluation Manager v2/FrmEvaluation.cs
@@ -0,0 +1,37 @@
+﻿using Evaluation_Manager.Models;
+using Evaluation_Manager.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluation_Manager {
+    public partial class FrmEvaluation : Form {
+        private StudentRepository selectedStudent;
+
+        public FrmEvaluation() {
+            InitializeComponent();
+        }
+
+        public FrmEvaluation(StudentRepository selectedStudent) {
+            this.selectedStudent = selectedStudent;
+        }
+
+        private void FrmEvaluation_Load(object sender, EventArgs e) {
+            SetFormText();
+            var activities = Activity()
+
+        }
+
+        private void SetFormText() {
+            Text = student.FirstName + " " + student.LastName;
+        }
+
+        private void 
+    }
+}
